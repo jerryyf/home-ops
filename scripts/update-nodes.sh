402ad08552f7414e9ec19f05d4e0b6b6b5bded2a
jerryyf <jerry@jerryyf.dev>
2026-08-28 20:59:52 +1000

    feat: nfd addon

--- scripts/update-nodes.sh
+++ scripts/update-nodes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Update all k3s nodes
+# Update all nodes
 
 ssh -t k3s-master "sudo apt update && sudo apt upgrade -y"
 ssh -t k3s-worker-1 "sudo apt update && sudo apt upgrade -y"
+ssh -t k3s-worker-2 "sudo apt update && sudo apt upgrade -y"
+ssh -t srv-1 "sudo apt update && sudo apt upgrade -y"
